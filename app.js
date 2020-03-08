62ceecbd39fd4d4765af1522ab27c329421dc5ee
gcoutini <gabriel_hb@hotmail.com>
2020-03-08 19:49:26 -0300

    prepara aplicacao para deploy no heroku usando mongo atlas

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const app = require('./core/express');
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000...');
+process.env.PORT = process.env.PORT || 3000;
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}...`);
 });
--- core/mongoose.js
+++ core/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost:27017/lojinha', { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(`mongodb+srv://lojinha:${process.env.DB_PASSWORD}@cluster0-mgg45.mongodb.net/lojinha?retryWrites=true&w=majority`, 
+        { useNewUrlParser: true, useUnifiedTopology: true });
     
     mongoose.connection.on('open', () => {
         console.log('Conectado ao banco de dados');
