a1579c3e3d634d2fa2a35e70d47834583bec8e9c
gcoutini <gabriel_hb@hotmail.com>
2020-03-08 19:33:40 -0300

    finaliza migracao para mongodb

--- resources/controllers/pages.js
+++ resources/controllers/pages.js
@@ -14,9 +14,9 @@ const index = (req, res) => {
 
 const home = (req, res) => {
     let user = req.session.user;
-
+    
     if(user) {
-        res.render('home', {opp:req.session.opp, name:user.fullname});
+        res.render('home', {opp:req.session.opp, name:user.fullName});
         return;
     }
     res.redirect('/');
@@ -34,23 +34,6 @@ const login = async (req, res) => {
     res.redirect('/home');
 };
 
-const login2 = (req, res) => {
-    // The data sent from the user are stored in the req.body.object
-    // call our login function and it will return the result(the user data)
-    user.login(req.body.username, req.body.password, function(result) {
-        if(result) {
-            // Store the user data in a session.
-            req.session.user = result;
-            req.session.opp = 1;
-            // redirect the user to the home page.
-            res.redirect('/home');
-        }else {
-            // if the login function returns null send this error message back to the user.
-            res.send('Username/Password incorrect!');
-        }
-    })
-}
-
 const register = async (req, res) => {
     req.body.password = await bcryptjs.hash(req.body.password, 10);
 
@@ -60,10 +43,14 @@ const register = async (req, res) => {
         password: req.body.password
     };
 
-    const user = await User.create(userInput);
-    req.session.user = user;
-    req.session.opp = 0;
-    res.redirect('/home');
+    try {
+        const user = await User.create(userInput);
+        req.session.user = user;
+        req.session.opp = 0;
+        res.redirect('/home');
+    } catch (err) {
+        err.name === 'MongoError' ? res.send('Usuário já existe') : err.errmsg;
+    }
 };
 
 const logout = (req, res) => {
